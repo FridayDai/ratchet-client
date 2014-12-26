8065cf5d14e6ad33794c8a7be432aebc3ad2b0c6
Albert Zhang <albert.zhang@xplusz.com>
2014-12-26 19:40:06 +0800

    Adding scss lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
 var gulp = require('gulp');
 
 var paths = {
+    sass: ['./grails-app/assets/stylesheets/**/*.scss'],
     js: [
         './grails-app/assets/javascripts/**/*.js',
         '!./grails-app/assets/javascripts/libs/**/*.js'
     ]
 };
 
+// JS hint for checking JS code
 var jshint = require('gulp-jshint');
 var jshintStylish = require('jshint-stylish');
 var jshintConfigPath = '.jshintrc';
 
-gulp.task('lint', function () {
+gulp.task('js-lint', function () {
     return gulp.src(paths.js)
         .pipe(jshint(jshintConfigPath))
         .pipe(jshint.reporter(jshintStylish));
 });
+
+// Scss lint for checking JS code
+var scsslint = require('gulp-scss-lint');
+
+gulp.task('scss-lint', function() {
+    return gulp.src(paths.sass)
+        .pipe(scsslint());
+});
+
+// This lint task should be run before commit code.
+gulp.task('lint', ['js-lint', 'scss-lint']);
