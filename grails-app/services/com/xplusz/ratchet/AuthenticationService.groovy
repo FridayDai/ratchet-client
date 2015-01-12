7d3c7beeafd2bb6f1818d02c8632621f2b16805c
sid.feng <sid.feng@xplusz.com>
2015-01-12 14:07:13 +0800

    RATTWO-155 provider desktop set up

--- grails-app/services/com/xplusz/ratchet/AuthenticationService.groovy
+++ grails-app/services/com/xplusz/ratchet/AuthenticationService.groovy
@@ -3,7 +3,7 @@ package com.xplusz.ratchet
 import com.mashape.unirest.http.Unirest
 import exceptions.AccountValidationException
 import grails.converters.JSON
-import net.sf.cglib.core.Local
+import java.util.Locale
 
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -28,11 +28,11 @@ class AuthenticationService {
 
     def authenticate(HttpServletRequest request, HttpServletResponse response, params) throws AccountValidationException {
 
-        def username = params.username
+        def email = params.email
         def password = params.password
 
-        if (!(username && password)) {
-            def errorMessage = messageSource.getMessage("security.errors.login.missParams", null, null)
+        if (!(email && password)) {
+            def errorMessage = messageSource.getMessage("security.errors.login.missParams", null, Locale.default)
             throw new AccountValidationException(errorMessage)
         }
 
@@ -41,6 +41,8 @@ class AuthenticationService {
          *
          * @param username
          * @param password
+         * @param client_platform
+         * @param client_type
          *
          * @requestMethod post
          *
@@ -48,24 +50,34 @@ class AuthenticationService {
          */
         def url = grailsApplication.config.ratchetv2.server.login.url
         def resp = Unirest.post(url)
-                .field("username", username)
+                .field("email", email)
                 .field("password", password)
+                .field("clientPlatform", grailsApplication.config.ratchetv2.server.client_platform)
+                .field("clientType", grailsApplication.config.ratchetv2.server.client_type)
                 .asString()
         def result = JSON.parse(resp.body)
 
-        if (resp?.status == 200) {
-            request.session.uid = result.sessionId
-            request.session.identifier = UUID.randomUUID().toString()
+        if (resp.status == 200) {
+            request.session.token = result.token
             def data = [
                     authenticated: true,
             ]
-
             return data
+        }
+
+        if (resp.status == 403) {
+            def rateLimit = result?.error?.errorMessage
+            Integer[] args = [rateLimit]
+            def errorMessage = messageSource.getMessage("security.errors.login.rateLimit", args, Locale.default)
+            throw new AccountValidationException(errorMessage, rateLimit)
+
+
         } else {
-            def errorMessage = result.error.errorMessage
+            def errorMessage = result?.error?.errorMessage
             throw new AccountValidationException(errorMessage)
 
         }
+
 
     }
 
@@ -78,26 +90,29 @@ class AuthenticationService {
      */
     def logout(request, response) {
         def session = request.session
-        def uid = session?.uid
-
-
+        def token = session?.token
         /**
          * Call backend logout api
          *
-         * @param sessionId
+         * @param token
          *
          * @requestMethod get
          */
+        if (!token) {
+            log.error("There is no token.")
+            return false
+        }
+
         def url = grailsApplication.config.ratchetv2.server.logout.url
         def resp = Unirest.get(url)
-                .queryString("sessionId", "${uid}")
+                .header("X-Auth-Token", "${token}")
                 .asString()
-        if (!uid || resp.status != 200) {
+        if (resp.status != 200) {
             log.warn("No user login in the session.")
             return false
         }
 
-        log.info("Logout $uid")
+        log.info("Logout ${token}")
         session.invalidate()
         return true
     }
---
+++ grails-app/views/shared/_nav.gsp
@@ -0,0 +1,23 @@
+<div class="nav">
+    <ul id="menu" class="list">
+        <li <g:if test="${ controllerName == 'home'}">class="nav-li active"</g:if>
+            <g:else>class="nav-li"</g:else>>
+            <g:link controller="home" action="index">
+                <div class="ui-icon icon-provider"></div>
+                <span>PROVIDERS</span>
+            </g:link>
+        </li>
+        <li <g:if test="${controllerName == 'patients'}">class="nav-li active"</g:if><g:else>class="nav-li"</g:else>>
+            <g:link controller="patients" action="index">
+                <div class="ui-icon icon-tool"></div>
+                <span>PATIENTS</span>
+            </g:link>
+        </li>
+        <li <g:if test="${controllerName == 'accounts'}">class="nav-li active"</g:if> <g:else>class="nav-li"</g:else>>
+            <g:link controller="accounts" action="index">
+                <div class="ui-icon icon-practice"></div>
+                <span>ACCOUNTS</span>
+            </g:link>
+        </li>
+    </ul>
+</div>
