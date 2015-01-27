8ba1f9e55367deae1a4261ef0f7773b399c0b0be
colin <colin.chen@xplusz.com>
2015-01-27 17:24:45 +0800

    RATTWO-239 #comment change taskController and add medicalRecordService and toolService

--- grails-app/controllers/com/xplusz/ratchet/TaskController.groovy
+++ grails-app/controllers/com/xplusz/ratchet/TaskController.groovy
@@ -2,38 +2,67 @@ package com.xplusz.ratchet
 
 class TaskController extends BaseController {
 
-    //    def beforeInterceptor = [action: this.&auth]
-
-    def index() {
-        redirect(uri: '/treatment/task');
-    }
+    def beforeInterceptor = [action: this.&auth]
 
     def taskService
+    def toolService
+    def medicalRecordService
 
-    def getTasks() {
-        def tasks = taskService.loadTasks()
-        def taskOverdue = []
-        def taskNew = []
-        def taskFuture = []
-        def taskCompleted = []
+    def index() {
+        redirect(uri: '/patients/x12345/treatments/3/21/tasks');
+    }
+
+//    def getTasks() {
+//        def tasks = taskService.loadTasks()
+//        def taskOverdue = []
+//        def taskNew = []
+//        def taskFuture = []
+//        def taskCompleted = []
+//        for (task in tasks) {
+//            if (task.status == "overdue") {
+//                taskOverdue.add(task)
+//                continue
+//            }
+//            if (task.status == "new") {
+//                taskNew.add(task)
+//                continue
+//            }
+//            if (task.status == "future") {
+//                taskFuture.add(task)
+//                continue
+//            }
+//            if (task.status == "complete") {
+//                taskCompleted.add(task)
+//            }
+//
+//        }
+//        render view: 'task', model: [taskOverdue: taskOverdue, taskNew: taskNew, taskFuture: taskFuture, taskCompleted: taskCompleted]
+//    }
+
+    def getTasksAndTools() {
+        def patientId = params.patientId
+        def medicalRecordId = params.medicalRecordId
+        def treatmentId = params.treatmentId
+
+        def tasks = medicalRecordService.showTasksByMedicalRecord(medicalRecordId)
+        def sentTasks = []
+        def scheduleTasks = []
         for (task in tasks) {
-            if (task.status == "overdue") {
-                taskOverdue.add(task)
+            if (isSent) {
+                sentTasks.add(task)
                 continue
             }
-            if (task.status == "new") {
-                taskNew.add(task)
-                continue
+            if (notsent) {
+                scheduleTasks.add(task)
             }
-            if (task.status == "future") {
-                taskFuture.add(task)
-                continue
-            }
-            if (task.status == "complete") {
-                taskCompleted.add(task)
-            }
-
         }
-        render view: 'task', model: [taskOverdue: taskOverdue, taskNew: taskNew, taskFuture: taskFuture, taskCompleted: taskCompleted]
+
+        def tools = toolService.getToolsByTreatment(treatmentId)
+        render view: 'task', model: [sentTasks: sentTasks, scheduleTasks:scheduleTasks, tools: tools]
+    }
+
+    def addTaskToMedicalRecord() {
+        def result = medicalRecordService.assignTaskToMedicalRecord(params)
+        return result
     }
 }
