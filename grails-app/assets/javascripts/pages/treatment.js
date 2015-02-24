6736ad317d90a4f990b22fd9a2aa01cd0dcf72e7
colin <colin.chen@xplusz.com>
2015-02-24 18:38:52 +0800

    RATTWO-384 #comment change treatment.js

--- grails-app/assets/javascripts/pages/treatment.js
+++ grails-app/assets/javascripts/pages/treatment.js
@@ -120,7 +120,7 @@
                 ui.jqXHR.error(function () {
                     ui.panel.html(RC.constants.errorMessage);
                 });
-                RC.common.progress(false);
+                RC.common.progress(true);
             },
             load: function (event, ui) {
                 var type = ui.tab.data("type");
@@ -131,16 +131,18 @@
                             break;
                         case "Task":
                             RC.pages.task.init(ui.panel);
+                            RC.common.progress(false);
                             break;
                         case "Team":
                             RC.pages.team.init(ui.panel);
+                            RC.common.progress(false);
                             break;
                         case "Overview":
                             RC.pages.overview.init(ui.panel);
                             break;
                     }
                 }
-                RC.common.progress(true);
+
             },
             disabled: [4, 5]
 
