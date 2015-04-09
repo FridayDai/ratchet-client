2daf8bb0f6ed8411c5bec68e476d7b5b2dbe03b0
colin <colin.chen@xplusz.com>
2015-04-09 14:22:59 +0800

    RH-626 #comment add groups controller and service

--- grails-app/controllers/com/ratchethealth/client/GroupsController.groovy
+++ grails-app/controllers/com/ratchethealth/client/GroupsController.groovy
@@ -1,28 +1,32 @@
 package com.ratchethealth.client
 
+import grails.converters.JSON
+
 class GroupsController extends BaseController {
 
     def beforeInterceptor = [action: this.&auth]
     def groupService
 
     def index() {
-        def groupList = groupService.getGroups(params)
-        render view:'groups', model: [groupList: groupList, pagesize: params.length]
+        def groupList = groupService.showGroupsList(request, response, params)
+        render view: 'groups', model: [groupList: groupList, pagesize: params.length]
     }
 
     def getGroups() {
-
+        def resp = groupService.showGroupsList(request, response, params)
+        render resp as JSON
     }
 
     def addGroup() {
-
+        def resp = groupService.createGroup(request, response, params)
+        def result = [resp: resp]
+        render result as JSON
     }
 
     def updateGroup() {
-
+        def resp = groupService.updateGroup(request, response, params)
+        def result = [resp: resp]
+        render result as JSON
     }
 
-    def deleteGroup() {
-
-    }
 }
