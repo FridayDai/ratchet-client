4d5baab2cef7603374fc398490ea9f42e7b4024f
Colin Chen <colin.chen@xplusz.com>
2015-07-14 17:02:28 +0800

    RH #comment add invitation and patient test

--- test/unit/com/ratchethealth/client/BulkImportServiceSpec.groovy
+++ test/unit/com/ratchethealth/client/BulkImportServiceSpec.groovy
@@ -1,5 +1,6 @@
 package com.ratchethealth.client
 
+import com.mashape.unirest.request.GetRequest
 import com.mashape.unirest.request.body.MultipartBody
 import com.ratchethealth.client.exceptions.ApiReturnException
 import grails.test.mixin.TestFor
@@ -18,8 +19,8 @@ class BulkImportServiceSpec extends Specification {
         }
 
         def importFile = [
-            'fileItem': ['fileName': 'hello'],
-            'transferTo': { file -> return true }
+                'fileItem'  : ['fileName': 'hello'],
+                'transferTo': { file -> return true }
         ]
 
         MultipartBody.metaClass.asString = { ->
@@ -40,7 +41,7 @@ class BulkImportServiceSpec extends Specification {
         given:
 
         def importFile = [
-                'fileItem': ['fileName': 'hello'],
+                'fileItem'  : ['fileName': 'hello'],
                 'transferTo': { file -> return true }
         ]
 
@@ -63,7 +64,7 @@ class BulkImportServiceSpec extends Specification {
         given:
 
         def importFile = [
-                'fileItem': ['fileName': 'hello'],
+                'fileItem'  : ['fileName': 'hello'],
                 'transferTo': { file -> return true }
         ]
 
@@ -81,5 +82,124 @@ class BulkImportServiceSpec extends Specification {
         ApiReturnException e = thrown()
         e.getMessage() == "body"
     }
+
+    def "test lookup with successful result"() {
+        given:
+        def jBuilder = new JsonBuilder()
+        jBuilder {
+            totalCount 2
+            items 1, 2
+        }
+
+        def bulkPagination = new BulkPagination()
+
+        MultipartBody.metaClass.asString = { ->
+            return [
+                    status: 200,
+                    body  : jBuilder.toString()
+            ]
+        }
+
+        when:
+        def result = service.lookup('token', 1, bulkPagination)
+
+        then:
+        result.recordsTotal == 2
+        result.recordsFiltered == 2
+        result.data == [1, 2]
+    }
+
+    def "test lookup without successful result"() {
+        given:
+        def bulkPagination = new BulkPagination()
+        MultipartBody.metaClass.asString = { ->
+            return [
+                    status: 400,
+                    body  : "body"
+            ]
+        }
+
+        when:
+        service.lookup('token', 1, bulkPagination)
+
+        then:
+        ApiReturnException e = thrown()
+        e.getMessage() == "body"
+    }
+
+    def "test downloadErrors with success result"() {
+        given:
+        def jBuilder = new JsonBuilder()
+        jBuilder {
+            errorFilePath 2
+        }
+
+        GetRequest.metaClass.asString = { ->
+            return [
+                    status: 200,
+                    body  : jBuilder.toString()
+            ]
+        }
+
+        when:
+        def result = service.downloadErrors('token', 1)
+
+        then:
+        result == 2
+    }
+
+    def "test downloadErrors without successful result"() {
+        given:
+        GetRequest.metaClass.asString = { ->
+            return [
+                    status: 400,
+                    body  : "body"
+            ]
+        }
+
+        when:
+        service.downloadErrors('token', 1)
+
+        then:
+        ApiReturnException e = thrown()
+        e.getMessage() == "body"
+    }
+
+    def "test savePatients with successful result"() {
+        given:
+
+        MultipartBody.metaClass.asString = { ->
+            return [
+                    status: 200,
+                    body  : 'body'
+            ]
+        }
+
+        when:
+        def result = service.savePatients('token', 1, 2)
+
+        then:
+        result == [:]
+
+    }
+
+    def "test savePatients without successful result"() {
+
+        given:
+        MultipartBody.metaClass.asString = { ->
+            return [
+                    status: 400,
+                    body  : "body"
+            ]
+        }
+
+        when:
+        service.savePatients('token', 1, 2)
+
+        then:
+        ApiReturnException e = thrown()
+        e.getMessage() == "body"
+    }
+
 
 }
---
+++ test/unit/com/ratchethealth/client/InvitationServiceSpec.groovy
@@ -0,0 +1,78 @@
+package com.ratchethealth.client
+
+import com.mashape.unirest.request.GetRequest
+import com.ratchethealth.client.exceptions.ApiReturnException
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+@TestFor(InvitationService)
+class InvitationServiceSpec extends Specification {
+
+    def "test invitePatient with successful result"() {
+        given:
+
+        GetRequest.metaClass.asString = { ->
+            return [
+                    status: 200,
+                    body  : true
+            ]
+        }
+
+        when:
+        def result = service.invitePatient('token', 1)
+
+        then:
+        result == true
+    }
+
+    def "test getSingleAccount without successful result"() {
+        given:
+        GetRequest.metaClass.asString = { ->
+            return [
+                    status: 400,
+                    body  : "body"
+            ]
+        }
+
+        when:
+        service.invitePatient('token', 1)
+
+        then:
+        ApiReturnException e = thrown()
+        e.getMessage() == "body"
+    }
+
+    def "test inviteAccount with successful result"() {
+        given:
+
+        GetRequest.metaClass.asString = { ->
+            return [
+                    status: 200,
+                    body  : true
+            ]
+        }
+
+        when:
+        def result = service.inviteAccount('token', 1)
+
+        then:
+        result == true
+    }
+
+    def "test inviteAccount without successful result"() {
+        given:
+        GetRequest.metaClass.asString = { ->
+            return [
+                    status: 400,
+                    body  : "body"
+            ]
+        }
+
+        when:
+        service.inviteAccount('token', 1)
+
+        then:
+        ApiReturnException e = thrown()
+        e.getMessage() == "body"
+    }
+}
