f5919b376365b9f4d5c6b415af0cdd24fbf9f45c
John Xu <john.xu@xplusz.com>
2015-08-13 16:52:58 +0800

    RH #comment Run gulp js-lint

--- grails-app/assets/javascripts/partials/base.js
+++ grails-app/assets/javascripts/partials/base.js
@@ -70,8 +70,7 @@
         return this.optional(element) || (tel.test(value));
     }, 'Please enter a valid phone number');
 
-    jQuery.validator.addMethod('checkPhoneNumberRegion', function (value, element) {
-        var PNF = libphonenumber.PhoneNumberFormat;
+    jQuery.validator.addMethod('checkPhoneNumberRegion', function (value) {
         var phoneUtil = libphonenumber.PhoneNumberUtil.getInstance();
         var phoneNumber = phoneUtil.parse(value, 'US');
         var isPossible = phoneUtil.isPossibleNumber(phoneNumber);
