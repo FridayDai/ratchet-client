f41edf70c0122ae3fb76364bd03894c2226071d8
thomas cai <thomas.cai@xplusz.com>
2016-01-18 11:30:59 +0800

    RH-2090 #comment email input endWith like ocm should not pop up an error dialog

--- grails-app/assets/javascripts/components/shared/validation/PatientEmailValidation.js
+++ grails-app/assets/javascripts/components/shared/validation/PatientEmailValidation.js
@@ -3,11 +3,26 @@ require('../../../libs/jquery-validation/jquery.validate.js');
 var STRINGs = require('../../../constants/Strings');
 var URLs = require('../../../constants/Urls');
 
+
+
+$.validator.addMethod("emailCheck", function(value){
+    var regex = /(cmo){1}$|(ocm){1}$|(omc){1}$|(mco){1}$|(moc){1}$/;
+
+    if (regex.test(value) == true) {
+        return false
+    } else {
+        return true
+    }
+
+}, STRINGs.EMAIL_INVALID);
+
+
 module.exports = {
     get: function (originalCheck, scope) {
         return {
             rules: {
                 email: {
+                    emailCheck: true,
                     email: true,
                     remote: {
                         url: URLs.CHECK_PATIENT_EMAIL,
