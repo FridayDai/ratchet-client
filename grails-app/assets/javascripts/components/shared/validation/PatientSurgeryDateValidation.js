3984ca3d19aadce13e60a974e16c7b53b3f05f17
jinfeng <albert.zhang@xplusz.com>
2016-02-26 11:06:57 +0800

    RH-2304 #comment Add pehapes format mapping for surgery date

--- grails-app/assets/javascripts/components/shared/validation/PatientSurgeryDateValidation.js
+++ grails-app/assets/javascripts/components/shared/validation/PatientSurgeryDateValidation.js
@@ -2,16 +2,29 @@ require('../../../libs/jquery-validation/jquery.validate.js');
 
 var STRINGs = require('../../../constants/Strings');
 var moment = require('moment');
+var Utility = require('../../../utils/Utility');
 
 var SURGERY_DATE_FORMAT = 'MMMM D, YYYY';
 
 $.validator.addMethod('SurgeryDateMinCheck', function (value) {
-    return !moment(value, SURGERY_DATE_FORMAT).isBefore('2015-1-1');
+    var guessFormat = Utility.guessDateFormat(value);
+
+    if (guessFormat) {
+        return !moment(value, guessFormat).isBefore('2015-01-01');
+    }
+
+    return false;
 }, STRINGs.MINIMUM_SURGERY_DATE);
 
 $.validator.addMethod('SurgeryDateMaxCheck', function (value) {
-    return moment(value, SURGERY_DATE_FORMAT)
+    var guessFormat = Utility.guessDateFormat(value);
+
+    if (guessFormat) {
+        return moment(value, guessFormat)
             .isBefore(moment().add(1, 'y'));
+    }
+
+    return false;
 }, STRINGs.MAX_SURGERY_DATE);
 
 module.exports = {
