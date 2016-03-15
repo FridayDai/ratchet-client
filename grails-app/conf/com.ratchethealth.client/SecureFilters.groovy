ab888f824c3610f323986e59b88cc4bb83d55e5c
Colin Chen <colin.chen@xplusz.com>
2016-03-15 16:04:57 +0800

    RH-2423 #comment security headers fix part2.

--- grails-app/conf/com.ratchethealth.client/SecureFilters.groovy
+++ grails-app/conf/com.ratchethealth.client/SecureFilters.groovy
@@ -11,8 +11,7 @@ class SecureFilters {
             after = { Map model ->
                 def cdnDomain = grailsApplication.config?.cdn_domain
                 if (response?.contentType?.indexOf('text/html') == 0) {
-                    response.setHeader('Cache-Control', 'private, max-age=0, no-cache, no-store')
-                    response.setHeader('Pragma', 'no-store')
+                    response.setHeader('Cache-Control', 'no-store')
                     response.setHeader('X-Frame-Options', 'DENY')
                     response.setHeader('X-XSS-Protection', '1;mode=block')
                     response.setHeader('X-Content-Type-Options', 'nosniff')
