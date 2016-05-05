3aab76ec192fe1fb99c7f8ba06912b4d6b4236ff
jinfeng <albert.zhang@xplusz.com>
2016-05-05 16:57:17 +0800

    RH-2079 #comment Fix jslint error

--- grails-app/assets/javascripts/components/patientDashboard/PatientLevelTabToolbar.js
+++ grails-app/assets/javascripts/components/patientDashboard/PatientLevelTabToolbar.js
@@ -113,7 +113,7 @@ function PatientLevelTabToolbar() {
     };
 
     this.onPatientInfoServed = function (e, data) {
-        this.updateNotifyStatus(this.hasActiveTasks, (data.email && data.emailStatus == '3'));
+        this.updateNotifyStatus(this.hasActiveTasks, (data.email && data.emailStatus === '3'));
     };
 
     this.onPatientInfoUpdateSuccess = function (e, data) {
